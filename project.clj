39217c621450dcbfecccdb7445aca0d2ba115ad3
Peter Taoussanis <peter@taoensso.com>
2021-01-11 13:11:33 +0100

    Bump deps

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                 *assert*             true}
 
   :dependencies
-  [[com.taoensso/encore "2.126.2"]]
+  [[com.taoensso/encore "3.10.1"]]
 
   :plugins
   [[lein-pprint    "1.3.2"]
@@ -22,13 +22,13 @@
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
-   :provided {:dependencies [[org.clojure/clojure       "1.7.0"]
-                             [org.clojure/clojurescript "1.9.473"]
+   :provided {:dependencies [[org.clojure/clojure       "1.10.1"]
+                             [org.clojure/clojurescript "1.10.773"]
                              [reagent                   "0.6.0"]]}
    :1.8      {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9      {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10     {:dependencies [[org.clojure/clojure "1.10.1"]]}
-   :test     {:dependencies [[org.clojure/test.check "0.9.0"]]}
+   :test     {:dependencies [[org.clojure/test.check "1.1.0"]]}
    :depr     {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]}
    :dev      [:1.10 :test :server-jvm :depr]}
 
@@ -46,9 +46,14 @@
       :pretty-print false}}]}
 
   :aliases
-  {"build-once" ["cljsbuild" "once"]
+  {"start-dev"  ["with-profile" "+dev" "repl" ":headless"]
    "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"]
-   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]}
+   "build-once" ["cljsbuild" "once"]
+   "test-cljs"  ["cljsbuild" "test"]
+   "test-all"
+   ["do" ["clean"]
+    "with-profile" "+1.10:+1.9" "test,"
+    "with-profile" "+test" "cljsbuild" "test"]}
 
   :repositories
   {"sonatype-oss-public"
